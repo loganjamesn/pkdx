8a440cf6ab3feb43ee1c56008ca6d43f8b909e94
Logan Nottbohm <logan@Lappy.local>
2022-06-09 20:43:11 -0400

    LN - versioning fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { PokemonsContainer } from "./containers/PokemonsContainer";
+
 import './App.scss';
 
 function App() {
+
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.now.sh'
+  });
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <ApolloProvider client={client}>
+        <main>
+          <PokemonsContainer />
+        </main>
+      </ApolloProvider>
       
     </div>
   );
